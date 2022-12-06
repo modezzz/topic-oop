f89fc2c1801aab64b63a82688b358b9cee043416
modezzz <andrey.gorbanyov7@gmail.com>
2022-12-06 20:08:05 +0200

    Head

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Challenge:
+
+#. Implement ``Product`` class
+#. Each ``Product`` instance should implement properties:
+    * ``name`` - a product's name, like apple, cheese etc.
+    * ``price`` - a price for a single unit
+#. ``Product`` instance should have a behavior of calculating total
+   price for a specified quantity of goods
+#. Implement ``ShoppingCart`` class
+#. ``ShoppingCart`` instance should combine products instances and
+    corresponding purchased quantities.
+#. ``ShoppingCart`` instance should implement a method to calculate
+   the total price of entire cart.
+
+"""
